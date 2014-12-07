0122479531dc061272de5c72f52e44f004adaef6
Naoto Ishdia <maxfield.walker@gmail.com>
2014-12-08 01:26:48 +0900

    12月7日最終分

--- 3-1/Source.c
+++ 3-1/Source.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void RouteNavigate(int **array){
+void RouteNavigate(int **array, int n){
+	int *route = (int *)malloc(sizeof(int) * n);
+	if (route == NULL)
+		exit(0);
+	int i;
+
+	for (i = 0; i < n; i++){
+
+	}
+
+
 
 
 }
 
 int main(void){
 
-	int travel_time[6][6] = { { 0, 7, 3, 0, 0, 0 }, 
-							  { 7, 0, 5, 7, 0, 0 }, 
-							  { 3, 5, 0, 6, 4, 0 }, 
-							  { 0, 7, 6, 0, 3, 9 }, 
-							  { 0, 0, 4, 3, 0, 8 }, 
-							  { 0, 0, 0, 9, 8, 0 } };
+	int travel_time[6][6] =
+	{ { 0, 7, 3, 0, 0, 0 },
+	{ 7, 0, 5, 7, 0, 0 },
+	{ 3, 5, 0, 6, 4, 0 },
+	{ 0, 7, 6, 0, 3, 9 },
+	{ 0, 0, 4, 3, 0, 8 },
+	{ 0, 0, 0, 9, 8, 0 } };
 
 	int n = 6;
 	int i;
@@ -22,7 +34,9 @@ int main(void){
 		p[i] = travel_time[i];
 	}
 
-	printf("%d\n",p[0][1]);
+	RouteNavigate(p, n);
+
+
 
 	return 0;
 }
