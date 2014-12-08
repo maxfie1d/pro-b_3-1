0e8e19b84db66972c67aa7c7e6a24e2a19390ddc
Naoto Ishdia <maxfield.walker@gmail.com>
2014-12-08 21:49:37 +0900

    p_passedを削除し，route[]に統合

--- 3-1/Source.c
+++ 3-1/Source.c
@@ -1,35 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void RouteNavigate(int **p_travel_time, int **p_passed, int *route, int n, int start, int count) {
+void RouteNavigate(int **p_travel_time, int *route, int n, int start, int count) {
 
 	int i, j;
-	if (start == n - 1) {
-		for (i = 0; i < 6; i++)
-			printf("%d-",route[i]);
-		count--;
-		for (j = 0; j < 6; j++) {
-			p_passed[j][start] = 1;
-		}
-		p_passed[route[count - 1]][route[count]] = 0;
-		RouteNavigate(p_travel_time, p_passed, route, n, start, count);
-	}
-	else {
-		for (i = 0; i < n; i++) {
-			if (p_travel_time[start][i] > 0 && p_passed[start][i] != 0) {
-				route[++count] = i;
-				for (j = 0; j < n; j++)
-					p_passed[j][start] = 0;
-				printf("%d-", i + 1);
-				start = i;
-				RouteNavigate(p_travel_time, p_passed, route, n, start, count);
-			}
 
-		}
-		count--;
-		for (j = 0; j < 6;j++)
 
-		RouteNavigate(p_travel_time, p_passed, route, n, start, count);
+	for (i = 0; i < n; i++) {
+		if (p_travel_time[start][i] > 0 && route[count - 1] != -1)  {
+			count++;
+			route[count] = i;
+			for (j = 0; j < n; j++)
+			printf("%d-", i + 1);
+			start = i;
+			RouteNavigate(p_travel_time,route, n, start, count);
+		}
 	}
 
 	return 0;
@@ -62,7 +47,8 @@ int main(void) {
 	if (route == NULL) //ƒƒ‚ƒŠŠm•ÛƒGƒ‰[
 		exit(0);
 
-	route[0] = 0;
+	for (i = 0; i < n; i++)
+		route[i] = -1;
 
 	int **p_travel_time[6];
 
@@ -70,12 +56,7 @@ int main(void) {
 		p_travel_time[i] = travel_time[i];
 	}
 
-	int **p_passed[6];
-
-	for (i = 0; i < n; i++)
-		p_passed[i] = passed[i];
-
-	RouteNavigate(p_travel_time, p_passed, route, n, 0,0);
+	RouteNavigate(p_travel_time, route, n, 0, 0);
 
 	return 0;
 }
